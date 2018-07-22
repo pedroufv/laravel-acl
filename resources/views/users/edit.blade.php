a06ba8fd43480ebbfa32f947654e249ebc51ebc8
Pedro Augusto <pams.pedro@gmail.com>
2018-07-21 21:25:58 -0300

    ajuste para exibir mais roles

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -35,9 +35,9 @@
                         </div>
                         <div class="form-group row">
                             <label for="label" class="col-md-4 col-form-label text-md-right">@lang('admin.roles.index')</label>
-                            <div class="col-md-6">
+                            <div class="col-md-6" style="padding-top:5px">
                                 @foreach($roles as $role)
-                                    <div class="col-md-4">
+                                    <div class="col-md-6 float-left">
                                         <input type="checkbox" class="form-check-input" name="roles[]" id="{{ $role->id }}" @if(old("roles")) {{ in_array($role->id, old("roles")) ? 'checked="checked"' : ''}} @else {{$role->check ? 'checked="checked"' : '' }} @endif value="{{ $role->id }}">
                                         <label class="form-check-label" for="exampleCheck1">{{ $role->label }}</label>
                                     </div>
